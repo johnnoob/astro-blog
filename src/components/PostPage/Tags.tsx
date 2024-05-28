6d7a00c665d708b3bc1957509d0a55d21521b936
JohnGenius <johngenius@JohnGeniusdeMacBook-Pro.local>
2024-05-28 12:37:36 +0800

    hotfix postPage layout

--- src/components/PostPage/Tags.tsx
+++ src/components/PostPage/Tags.tsx
@@ -10,10 +10,10 @@ const Tags = ({ tags }: TagsProps) => {
   const [isShowTags, setIsShowTags] = useState<boolean>(false);
   const tagsLengthLimit = 3;
   return (
-    <div className="flex items-start gap-2 max-sm:flex-col max-sm:items-start">
+    <div className="flex items-start gap-2 text-sm max-sm:flex-col max-sm:items-start">
       {tags.length > tagsLengthLimit && (
         <button
-          className="flex items-center space-x-1 text-sm rounded-2xl px-2 py-1 bg-muted-foreground text-muted min-w-fit"
+          className="flex items-center space-x-1 rounded-2xl px-2 py-1 bg-muted-foreground text-muted min-w-fit"
           onClick={() => setIsShowTags(!isShowTags)}
         >
           <div className="flex items-center gap-1">
@@ -30,7 +30,7 @@ const Tags = ({ tags }: TagsProps) => {
             href={`/tags/${tag}`}
             className="bg-muted text-foreground border-[1px] text-sm rounded-2xl px-2 py-1 hover:bg-primary hover:text-muted"
             style={{
-              display: isShowTags || index < tagsLengthLimit ? "block" : "none",
+              display: isShowTags || index < tagsLengthLimit ? "" : "none",
             }}
           >
             {tag}
