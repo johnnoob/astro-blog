3470a6a618e550abf3f7c9f2f56a3b1a0f3d28cf
PracticeNoob <94057214+johnnoob@users.noreply.github.com>
2024-06-25 11:39:56 +0800

    Update views.ts

--- src/pages/api/views.ts
+++ src/pages/api/views.ts
@@ -47,7 +47,7 @@ export const GET: APIRoute = async ({ request }) => {
     status: 200,
     headers: {
       "content-type": "application/json",
-      "cache-control": "public, s-maxage=60, stale-while-revalidate=25",
+      "cache-control": "public, s-maxage=1, stale-while-revalidate=25",
     },
   });
 };
